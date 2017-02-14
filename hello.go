0bed3aa9d4e3a5bda1bf67dfa8991e33e9083104
Madhuri Yechuri <cosmokramer@gmail.com>
2017-02-13 21:04:50 -0800

    go fmt hello

--- hello.go
+++ hello.go
@@ -1,10 +1,10 @@
 package main
 
 import (
-        "runtime"
-        "fmt"
 	"C"
-        "github.com/lazytiger/go-v8"
+	"fmt"
+	"github.com/lazytiger/go-v8"
+	"runtime"
 )
 
 func main() {
@@ -12,14 +12,14 @@ func main() {
 
 //export GoMain
 func GoMain() {
-	fmt.Println("Hello, 世界");
-	fmt.Println("Go version:", runtime.Version());
-            engine := v8.NewEngine()
-    script := engine.Compile([]byte("'Hello ' + 'World!'"), nil, nil)
-    context := engine.NewContext(nil)
+	fmt.Println("Hello, 世界")
+	fmt.Println("Go version:", runtime.Version())
+	engine := v8.NewEngine()
+	script := engine.Compile([]byte("'Hello ' + 'World!'"), nil, nil)
+	context := engine.NewContext(nil)
 
-    context.Scope(func(cs v8.ContextScope) {
-        result := script.Run()
-        println(result.ToString())
-    })
+	context.Scope(func(cs v8.ContextScope) {
+		result := script.Run()
+		println(result.ToString())
+	})
 }
