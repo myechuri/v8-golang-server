3a7e54032bd9f5056b9add89ca78af60c3a1b2d2
Madhuri Yechuri <cosmokramer@gmail.com>
2017-02-14 15:51:44 -0800

    use ry/v8worker base

--- hello.go
+++ hello.go
@@ -3,23 +3,30 @@ package main
 import (
 	"C"
 	"fmt"
-	"github.com/lazytiger/go-v8"
+	"github.com/ry/v8worker"
 	"runtime"
 )
 
 func main() {
 }
 
+func DiscardSendSync(msg string) string { return "" }
+
 //export GoMain
 func GoMain() {
 	fmt.Println("Hello, 世界")
 	fmt.Println("Go version:", runtime.Version())
-	engine := v8.NewEngine()
-	script := engine.Compile([]byte("'Hello ' + 'World!'"), nil, nil)
-	context := engine.NewContext(nil)
 
-	context.Scope(func(cs v8.ContextScope) {
-		result := script.Run()
-		println(result.ToString())
-	})
+	worker := v8worker.New(func(msg string) {
+		println("recv cb", msg)
+		if msg != "hello" {
+			fmt.Println("bad msg", msg)
+		}
+	}, DiscardSendSync)
+
+	code := ` $print("ready"); `
+	err := worker.Load("code.js", code)
+	if err != nil {
+		fmt.Println(err.Error())
+	}
 }
